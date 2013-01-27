aef2acf02b48b50085dac14b079b36ebd0cfd8fb
Henric Trotzig <henric.trotzig@gmail.com>
2013-01-26 21:03:31 -0800

    Markdown support

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,2 +1,12 @@
 module BlogHelper
+
+  def markdown_to_html(markdown)
+    converter = Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML,
+      autolink: true,
+      space_after_headers: true
+    )
+
+    converter.render(markdown)
+  end
 end

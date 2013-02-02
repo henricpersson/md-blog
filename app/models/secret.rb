13f780a5526b7204d3ea6056a943643db4c04683
Henric Trotzig <henric.trotzig@gmail.com>
2013-02-02 13:35:05 -0800

    Switched to env variables for google api auth

--- app/models/secret.rb
+++
@@ -1,3 +0,0 @@
-class Secret < ActiveRecord::Base
-  attr_accessible :google_client_id, :google_client_secret
-end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  begin
-    secret = Secret.last
-    provider :google_oauth2,
-      secret.google_client_id,
-      secret.google_client_secret
-  rescue
-    Rails.logger.fatal <<-LOG
-      No secrets found. Make sure there is a single row in the
-      db table secrets. Without this, some features may not work.
-      LOG
-  end
+  provider :google_oauth2,
+    ENV['GOOGLE_CLIENT_ID'],
+    ENV['GOOGLE_CLIENT_SECRET']
 end

02c67c4a654b07265d5a82fd099319c023963d4e
Henric Trotzig <henric.trotzig@gmail.com>
2013-02-03 23:38:03 -0800

    Set default sort order

--- app/models/blog_entry.rb
+++ app/models/blog_entry.rb
@@ -2,6 +2,8 @@ class BlogEntry < ActiveRecord::Base
   attr_accessible :body, :published_at, :title, :url_slug
   acts_as_url :title, url_attribute: :url_slug
 
+  default_scope order: 'published_at DESC'
+
   def to_param
       "#{id}-#{url_slug}"
   end

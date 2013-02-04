4bbaa9c1309f5afeb89ff62bd4c5f6afed856165
Henric Trotzig <henric.trotzig@gmail.com>
2013-02-03 22:24:33 -0800

    Style changes and better truncation of text

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -14,7 +14,6 @@ module BlogHelper
 end
 class PygmentizeHTML < Redcarpet::Render::HTML
   def block_code(code, language)
-    require 'pygmentize'
     Pygmentize.process(code, language)
   end
 end

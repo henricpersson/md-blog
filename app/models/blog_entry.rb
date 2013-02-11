1c3461be62e1dd6e50abeb92fbbccc0f7c989f4a
Henric Trotzig <henric.trotzig@gmail.com>
2013-02-10 19:00:24 -0800

    Pre-render body html

--- app/models/blog_entry.rb
+++ app/models/blog_entry.rb
@@ -1,10 +1,17 @@
 class BlogEntry < ActiveRecord::Base
+  include BlogHelper
+
   attr_accessible :body, :published_at, :title, :url_slug, :rendered_body
   acts_as_url :title, url_attribute: :url_slug
+  before_save :generate_rendered_body
 
   default_scope order: 'published_at DESC'
 
   def to_param
       "#{id}-#{url_slug}"
+  end
+
+  def generate_rendered_body
+    self.rendered_body = markdown_to_html(body)
   end
 end
